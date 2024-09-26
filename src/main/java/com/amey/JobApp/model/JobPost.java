ba1a4eaa7675c6be0ceaf4107bdab779991afc88
Amey Bagwe <ameybagwe09@gmail.com>
2024-09-26 22:12:29 +0530

    Job App

---
+++ src/main/java/com/amey/JobApp/model/JobPost.java
@@ -0,0 +1,19 @@
+package com.amey.JobApp.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data // HENCE NO NEED TO CREATE GETTERS AND SETTERS
+@NoArgsConstructor // NO NEED TO CREATE CONSTRUCTOR
+@AllArgsConstructor
+@Component
+public class JobPost {
+    private int postId;
+    private int reqExperience;
+    private String postProfile;
+    private String postDesc;
+    private List<String> postTechStack;
+}
---
+++ src/test/java/com/amey/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amey.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
